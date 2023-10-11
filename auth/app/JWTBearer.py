5628ae02387b75f7730966e4b4c3b90c765c7e72
Stefan Weber <stefan.weber93@googlemail.com>
2023-10-11 13:24:27 +0200

    Imporve script add scope
    Signed-off-by: Stefan Weber <stefan.weber93@googlemail.com>

--- auth/app/JWTBearer.py
+++ auth/app/JWTBearer.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 import jwt
@@ -7,11 +9,12 @@ from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 # Configuration (Replace these with your actual configurations)
-JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 60
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 360000
 JWT_PRIVATE_KEY_PATH = "/Users/stefanwe/code/rddl/auth/private_key.pem"  # Replace with the path to your RSA private key
 JWT_PUBLIC_KEY_PATH = "/Users/stefanwe/code/rddl/auth/public_key.pem"  # Replace with the path to your RSA public key
 JWT_ALGORITHM = "RS256"
-JWT_DOMAIN = "localhost"  # Replace with your actual domain
+JWT_DOMAIN = "rddl.io"  # Replace with your actual domain
+
 
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
@@ -60,27 +63,51 @@ class JWTBearer(HTTPBearer):
                 )
 
     @staticmethod
+    def verify_client_id(jwt_token: str) -> bool:
+        decoded_token = JWTBearer.decodeJWT(jwt_token)
+        if decoded_token:
+            registered_public_key = JWTBearer._load_rsa_key(JWT_PUBLIC_KEY_PATH)
+            public_key = decoded_token["actor"]
+        if public_key == registered_public_key:
+            return True
+        return False
+
+    @staticmethod
     def signJWT(public_key: str) -> Dict[str, str]:
-        payload = {"actor": f"{public_key}@{JWT_DOMAIN}", "exp": time.time() + JWT_ACCESS_TOKEN_EXPIRE_MINUTES}
+        payload = {"actor": f"{public_key}@{JWT_DOMAIN}", "exp": time.time() + JWT_ACCESS_TOKEN_EXPIRE_MINUTES,
+                   "scope": "rabbitmq.write"}
         private_key = JWTBearer._load_rsa_key(JWT_PRIVATE_KEY_PATH, is_private=True)
 
         token = jwt.encode(payload, private_key, algorithm=JWT_ALGORITHM)
 
-        return JWTBearer.token_response(token)
+        # Calculate the expiring time and date of the JWT token.
+        exp = payload["exp"]
+        exp_datetime = datetime.fromtimestamp(exp)
+
+        return {"token": token, "exp_datetime": exp_datetime}
 
     @staticmethod
     def verify_token_payload(token: str) -> Optional[Dict]:
         decoded_token = JWTBearer.decodeJWT(token)
         if decoded_token:
             if "actor" in decoded_token and decoded_token["actor"].endswith(f"@{JWT_DOMAIN}"):
-                return decoded_token
+                # Calculate the expiring time and date of the JWT token.
+                exp = decoded_token["exp"]
+                exp_datetime = datetime.fromtimestamp(exp)
+
+                return {"decoded_token": decoded_token, "exp_datetime": exp_datetime}
+            return None
         return None
 
     @staticmethod
     def decodeJWT(token: str) -> dict:
-        if not token or token.count('.') != 2:
-            print("Invalid or ill-formed token")
-            return {}
+        try:
+            if not token or token.count('.') != 2:
+                print("Invalid or ill-formed token")
+                return {}
+        except Exception as e:
+            print(f"An error occurred while decoding the token: {e}")
+            raise HTTPBearer(description="Failed to decode JWT")
 
         public_key = JWTBearer._load_rsa_key(JWT_PUBLIC_KEY_PATH)
 
